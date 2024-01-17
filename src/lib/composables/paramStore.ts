2037f7d784bff4fd6b0914d7a95a0a1d9ad2a330
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-17 19:21:15 +0100

    feat: debounce

--- src/lib/composables/paramStore.ts
+++ src/lib/composables/paramStore.ts
@@ -3,13 +3,40 @@ import { goto } from '$app/navigation';
 import { page } from '$app/stores';
 import { get, writable } from 'svelte/store';
 
-export const useParamStore = <T>(name: string, fallback: T) => {
+interface ParamStoreOptions {
+	debounce?: number;
+}
+
+const defaultOptions: ParamStoreOptions = {};
+
+export const useParamStore = <T>(name: string, fallback: T, options: ParamStoreOptions) => {
 	const urlValue = get(page).url.searchParams.get(name);
 	const initValue = urlValue ? (urlValue as unknown as T) : fallback;
 
+	options = { ...defaultOptions, ...options };
+
 	const { subscribe, set, update } = writable<T>(initValue);
 
-	subscribe((value) => {
+	let timeoutId: number | null = null;
+	subscribe(async (value) => {
+		if (options.debounce) {
+			await waitForDelay(options.debounce);
+		}
+		updateParams(value);
+	});
+
+	async function waitForDelay(delay: number) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		return new Promise((resolve) => {
+			timeoutId = setTimeout(() => {
+				resolve(null);
+			}, delay) as unknown as number;
+		});
+	}
+
+	function updateParams(value: T) {
 		const params = get(page).url.searchParams;
 		if (value) {
 			params.set(name, `${value}`);
@@ -17,7 +44,7 @@ export const useParamStore = <T>(name: string, fallback: T) => {
 			params.delete(name);
 		}
 		navigate(`?${params.toString()}`);
-	});
+	}
 
 	function navigate(url: string) {
 		if (browser) {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { useParamStore } from '$lib/composables/paramStore';
 
-	let name = useParamStore<string>('name', '');
+	let name = useParamStore<string>('name', '', { debounce: 500 });
 	let search = useParamStore<string>('search', '');
+	let fruits = useParamStore<string>('fruits', []);
 </script>
 
 <h1>Welcome to your library project</h1>
@@ -20,6 +21,20 @@
 		<div>
 			<input type="text" placeholder="Search..." bind:value={$search} />
 			<p>{$search}</p>
+		</div>
+		<div>
+			<label>
+				<input type="checkbox" bind:group={$fruits} value="apples" />
+				Apples
+			</label>
+			<label>
+				<input type="checkbox" bind:group={$fruits} value="oranges" />
+				Oranges
+			</label>
+			<label>
+				<input type="checkbox" bind:group={$fruits} value="bananas" />
+				Bananas
+			</label>
 		</div>
 	</div>
 </section>
