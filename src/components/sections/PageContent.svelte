29ff50013b6fa1c8ca108e10c08688699d042c45
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-20 22:22:27 +0100

    feat: highlight section on scroll

--- src/components/sections/PageContent.svelte
+++ src/components/sections/PageContent.svelte
@@ -1,12 +1,96 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	const sections = [
+		{
+			id: 'installation',
+			title: 'Installation'
+		},
+		{
+			id: 'usage',
+			title: 'Usage'
+		},
+		{
+			id: 'debounce',
+			title: 'Debounce'
+		},
+		{
+			id: 'multiple',
+			title: 'Multiple'
+		},
+		{
+			id: 'update-programmatically',
+			title: 'Update Programmatically'
+		},
+		{
+			id: 'store-api',
+			title: 'Store API'
+		}
+	];
+
+	let options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 1
+	};
+
+	let elements: Element[] = [];
+	let activeElement: string = '';
+
+	onMount(() => {
+		elements = getElements(sections.map((section) => section.id));
+
+		let observer = initializeObserver(elements, (entries: IntersectionObserverEntry[]) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					activeElement = entry.target.id;
+				}
+			});
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	});
+
+	function initializeObserver(el: Element[], callback: IntersectionObserverCallback) {
+		let observer = new IntersectionObserver(callback, options);
+
+		if (browser) {
+			el.forEach((element) => {
+				observer.observe(element);
+			});
+		}
+
+		return observer;
+	}
+
+	function getElements(ids: string[]) {
+		let result: Element[] = [];
+		if (browser) {
+			ids.forEach((id) => {
+				let element = document.querySelector(`#${id}`);
+				if (element) {
+					result.push(element);
+				}
+			});
+		}
+
+		return result;
+	}
+</script>
+
 <div class="page-content">
 	<h2 class="title title--tertiary">Page Content</h2>
 	<ul class="list">
-		<li><a href="#installation">Installation</a></li>
-		<li><a href="#usage">Usage</a></li>
-		<li><a href="#debounce">Debounce</a></li>
-		<li><a href="#multiple">Multiple</a></li>
-		<li><a href="#update-programmatically">Update Programmatically</a></li>
-		<li><a href="#store-api">Store API</a></li>
+		{#each sections as section}
+			<li>
+				<a href={`#${section.id}`} class={activeElement === section.id ? 'active' : ''}
+					>{section.title}</a
+				>
+			</li>
+		{/each}
 	</ul>
 </div>
 
@@ -34,6 +118,10 @@
 		color: var(--color-foreground);
 		font-size: 0.9rem;
 		text-decoration: none;
+	}
+
+	.active {
+		color: var(--secondary);
 	}
 
 	@media (min-width: 1200px) {
