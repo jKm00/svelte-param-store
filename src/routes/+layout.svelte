c130e43913414a610d0636d6c756d7463eadadf7
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-01-20 02:00:04 +0100

    feat: seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,17 @@
 
 <svelte:head>
 	<title>Svelte Param Store</title>
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<meta name="description" content="A store helping you synchronize your app state and the URL." />
+	<meta name="keywords" content="svelte, store, url, param, query, state, sync, synchronize" />
+	<meta name="author" content="Joakim Edvardsen" />
+	<meta name="og:title" content="Svelte Param Store" />
+	<meta
+		name="og:description"
+		content="A store helping you synchronize your app state and the URL."
+	/>
+	<meta property="og:site_name" content="Svelte Param Store" />
+	<meta property="og:url" content="https://svelte-param-store.vercel.app" />
 </svelte:head>
 
 <slot />
